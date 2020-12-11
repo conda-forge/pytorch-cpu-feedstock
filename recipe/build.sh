d44a33fe6b31f94754e27a43e9846887aeecb47d
Isuru Fernando <isuruf@gmail.com>
2020-12-11 18:27:08 +0000

    try gpu on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -53,22 +53,28 @@ fi
 CPPFLAGS="${CPPFLAGS//-std=c++17/-std=c++14}"
 CXXFLAGS="${CXXFLAGS//-std=c++17/-std=c++14}"
 
-if [[ ${pytorch_variant} = "gpu" ]]; then
+if [[ ${cuda_compiler_version} != "None" ]]; then
     export USE_CUDA=1
     export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX"
     if [[ ${cudatoolkit} == 9.0* ]]; then
         export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;7.0"
     elif [[ ${cudatoolkit} == 9.2* ]]; then
         export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0"
-    elif [[ ${cudatoolkit} == 10.0* ]]; then
+    elif [[ ${cudatoolkit} == 10.* ]]; then
         export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5"
-    elif [[ ${cudatoolkit} == 10.1* ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5"
+    elif [[ $CUDA_VERSION == 11.0* ]]; then
+        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0"
+    elif [[ $CUDA_VERSION == 11.1* ]]; then
+        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6"
+    else
+        echo "unsupported cuda version. edit build.sh"
+        exit 1
     fi
     export TORCH_NVCC_FLAGS="-Xfatbin -compress-all"
-    export NCCL_ROOT_DIR=/usr/local/cuda
-    export USE_STATIC_NCCL=1
-    export CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda
+    export NCCL_ROOT_DIR=$PREFIX
+    export USE_STATIC_NCCL=0
+    export USE_STATIC_CUDNN=0
+    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
     export MAGMA_HOME="${PREFIX}"
 else
     export BLAS="MKL"
