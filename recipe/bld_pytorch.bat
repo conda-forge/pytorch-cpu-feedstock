bd8491671e134465d1b17ceabd117d137d833732
Isuru Fernando <isuruf@gmail.com>
2020-12-28 15:55:03 -0600

    Update bld_pytorch.bat

--- recipe/bld_pytorch.bat
+++ recipe/bld_pytorch.bat
@@ -4,32 +4,28 @@ set TH_BINARY_BUILD=1
 set PYTORCH_BUILD_VERSION=%PKG_VERSION%
 set PYTORCH_BUILD_NUMBER=%PKG_BUILDNUM%
 
-if "%pytorch_variant%" == "gpu" (
-    set build_with_cuda=1
-    set desired_cuda=%CUDA_VERSION:~0,-1%.%CUDA_VERSION:~-1,1%
-) else (
-    set build_with_cuda=
+if "%cuda_compiler_version%" == "None" (
     set USE_CUDA=0
 )
 
-if "%build_with_cuda%" == "" goto cuda_flags_end
+if "%cuda_compiler_version%" == "None" goto cuda_flags_end
 
-set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v%desired_cuda%
 set CUDA_BIN_PATH=%CUDA_PATH%\bin
 set TORCH_CUDA_ARCH_LIST=3.5;5.0+PTX
-if "%desired_cuda%" == "9.0" set TORCH_CUDA_ARCH_LIST=%TORCH_CUDA_ARCH_LIST%;6.0;7.0
-if "%desired_cuda%" == "9.2" set TORCH_CUDA_ARCH_LIST=%TORCH_CUDA_ARCH_LIST%;6.0;6.1;7.0
-if "%desired_cuda%" == "10.0" set TORCH_CUDA_ARCH_LIST=%TORCH_CUDA_ARCH_LIST%;6.0;6.1;7.0;7.5
+if "%cuda_compiler_version%" == "9.2" set TORCH_CUDA_ARCH_LIST=%TORCH_CUDA_ARCH_LIST%;6.0;6.1;7.0
+if "%cuda_compiler_version%" == "10.0" set TORCH_CUDA_ARCH_LIST=%TORCH_CUDA_ARCH_LIST%;6.0;6.1;7.0;7.5
+if "%cuda_compiler_version%" == "10.1" set TORCH_CUDA_ARCH_LIST=%TORCH_CUDA_ARCH_LIST%;6.0;6.1;7.0;7.5
+if "%cuda_compiler_version%" == "10.2" set TORCH_CUDA_ARCH_LIST=%TORCH_CUDA_ARCH_LIST%;6.0;6.1;7.0;7.5
+if "%cuda_compiler_version%" == "11.0" set TORCH_CUDA_ARCH_LIST=%TORCH_CUDA_ARCH_LIST%;6.0;6.1;7.0;7.5;8.0
+if "%cuda_compiler_version%" == "11.1" set TORCH_CUDA_ARCH_LIST=%TORCH_CUDA_ARCH_LIST%;6.0;6.1;7.0;7.5;8.0;8.6
 set TORCH_NVCC_FLAGS=-Xfatbin -compress-all
 
 :cuda_flags_end
 
-set DISTUTILS_USE_SDK=1
-
 set CMAKE_INCLUDE_PATH=%LIBRARY_PREFIX%\include
 set LIB=%LIBRARY_PREFIX%\lib;%LIB%
 
-IF "%build_with_cuda%" == "" goto cuda_end
+IF "%cuda_compiler_version%" == "None" goto cuda_end
 
 set MAGMA_HOME=%LIBRARY_PREFIX%
 
