b8fec19074e3def235419d8173c4d1555ac7c421
ngam <67342040+ngam@users.noreply.github.com>
2022-06-02 00:42:09 -0400

    trim

--- recipe/build_pytorch.sh
+++ recipe/build_pytorch.sh
@@ -83,19 +83,10 @@ export MAX_JOBS=${CPU_COUNT}
 
 if [[ ${cuda_compiler_version} != "None" ]]; then
     export USE_CUDA=1
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX"
-    if [[ ${cuda_compiler_version} == 9.0* ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;7.0"
-    elif [[ ${cuda_compiler_version} == 9.2* ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0"
-    elif [[ ${cuda_compiler_version} == 10.* ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5"
-    elif [[ ${cuda_compiler_version} == 11.0* ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0"
-    elif [[ ${cuda_compiler_version} == 11.1 ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6"
+    if [[ ${cuda_compiler_version} == 10.2 ]]; then
+        export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5+PTX"
     elif [[ ${cuda_compiler_version} == 11.2 ]]; then
-        export TORCH_CUDA_ARCH_LIST="8.0"
+        export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6+PTX"
     else
         echo "unsupported cuda version. edit build_pytorch.sh"
         exit 1
