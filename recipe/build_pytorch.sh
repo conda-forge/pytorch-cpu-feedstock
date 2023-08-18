b32c61d8b79d53341412ba9cf37a0f02a0428cd5
RaulPPealez <raulppelaez@gmail.com>
2023-08-18 08:20:45 +0200

    Add missing archs

--- recipe/build_pytorch.sh
+++ recipe/build_pytorch.sh
@@ -117,7 +117,7 @@ if [[ ${cuda_compiler_version} != "None" ]]; then
     elif [[ ${cuda_compiler_version} == 11.2 ]]; then
         export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
     elif [[ ${cuda_compiler_version} == 12.0 ]]; then
-        export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;9.0+PTX"
+        export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
     else
         echo "unsupported cuda version. edit build_pytorch.sh"
         exit 1
