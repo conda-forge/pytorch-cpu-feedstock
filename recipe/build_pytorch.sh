b9ba9e57976b44cd03c5647b400f75798040aacd
Mark Harfouche <mark.harfouche@gmail.com>
2023-08-21 08:00:36 -0400

    Update build_pytorch.sh

--- recipe/build_pytorch.sh
+++ recipe/build_pytorch.sh
@@ -80,7 +80,7 @@ if [[ "${target_platform}" != "${build_platform}" ]]; then
     rm -rf ${PREFIX}/bin/protoc-*
 
     cp ${BUILD_PREFIX}/bin/protoc ${PREFIX}/bin/protoc
-    cp ${BUILD_PREFIX}/bin/protoc-* ${PREFIX}/bin/protoc-*
+    cp ${BUILD_PREFIX}/bin/protoc-* ${PREFIX}/bin/
 fi
 
 # I don't know where this folder comes from, but it's interfering with the build in osx-64
