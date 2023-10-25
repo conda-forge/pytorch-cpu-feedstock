2b3578fb96f179bfa4a139885380786064cc340b
RaulPPealez <raulppelaez@gmail.com>
2023-10-25 18:13:47 +0200

    Add archs for CUDA 11.8 and CUDA 12.0 to build_pytorch.sh

--- recipe/build_pytorch.sh
+++ recipe/build_pytorch.sh
@@ -139,6 +139,10 @@ if [[ ${cuda_compiler_version} != "None" ]]; then
         export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
     elif [[ ${cuda_compiler_version} == 11.2 ]]; then
         export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
+    elif [[ ${cuda_compiler_version} == 11.8 ]]; then
+        export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
+    elif [[ ${cuda_compiler_version} == 12.0 ]]; then
+        export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
     else
         echo "unsupported cuda version. edit build_pytorch.sh"
         exit 1
