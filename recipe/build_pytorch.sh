b4d9ffb2a44c7e1dfad9dc3cf69693b4238145af
H. Vetinari <h.vetinari@gmx.com>
2024-05-17 19:44:33 +1100

    ensure $SP_DIR/torch exists

--- recipe/build_pytorch.sh
+++ recipe/build_pytorch.sh
@@ -1,6 +1,7 @@
 set -x
 if [[ "$megabuild" == true ]]; then
   source $RECIPE_DIR/build_libtorch.sh
+  mkdir -p $SP_DIR/torch
   pushd $SP_DIR/torch
   for f in bin/* lib/* share/* include/*; do
     if [[ -e "$PREFIX/$f" ]]; then
