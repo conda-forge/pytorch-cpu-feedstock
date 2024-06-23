9380df87e461c0b2f1256414e98b475481704d19
Mark Harfouche <mark.harfouche@gmail.com>
2024-06-23 12:59:52 -0400

    Address libomp runtime bugs for OSX

--- recipe/build_pytorch.sh
+++ recipe/build_pytorch.sh
@@ -1,4 +1,8 @@
 set -x
+# https://github.com/conda-forge/pytorch-cpu-feedstock/issues/243
+# https://github.com/pytorch/pytorch/blob/v2.3.1/setup.py#L341
+export PACKAGE_TYPE=conda
+
 if [[ "$megabuild" == true ]]; then
   source $RECIPE_DIR/build.sh
   # if $SP_DIR/torch doesn't exist here, the installation
