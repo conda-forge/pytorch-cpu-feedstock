4cf5872670d3a22aa400dbeb244be7eeb3677d86
Mark Harfouche <mark.harfouche@gmail.com>
2024-06-25 12:20:47 -0400

    just remove their libraries.

--- recipe/build_pytorch.sh
+++ recipe/build_pytorch.sh
@@ -1,19 +1,16 @@
 set -x
+source $RECIPE_DIR/build.sh
+
+# if $SP_DIR/torch doesn't exist here, the installation
+# of pytorch (see build_libtorch.sh call above) failed
+pushd $SP_DIR/torch
+
+# Remove the vendorered libraries they seem to include
 # https://github.com/conda-forge/pytorch-cpu-feedstock/issues/243
 # https://github.com/pytorch/pytorch/blob/v2.3.1/setup.py#L341
-export PACKAGE_TYPE=conda
-
-if [[ "$megabuild" == true ]]; then
-  source $RECIPE_DIR/build.sh
-  # if $SP_DIR/torch doesn't exist here, the installation
-  # of pytorch (see build_libtorch.sh call above) failed
-  pushd $SP_DIR/torch
-  for f in bin/* lib/* share/* include/*; do
-    if [[ -e "$PREFIX/$f" ]]; then
-      rm -rf $f
-      ln -sf $PREFIX/$f $PWD/$f
-    fi
-  done
-else
-  $PREFIX/bin/python -m pip install --no-deps --no-cache-dir torch-*.whl
-fi
+for f in bin/* lib/* share/* include/*; do
+  if [[ -e "$PREFIX/$f" ]]; then
+    rm -rf $f
+    ln -sf $PREFIX/$f $PWD/$f
+  fi
+done
