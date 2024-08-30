d1dacc1e85d05e68451fdba7cb33e92b24e0811b
Mark Harfouche <mark.harfouche@gmail.com>
2024-08-30 07:24:40 -0400

    debug things

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,8 @@ set -ex
 # https://github.com/pytorch/pytorch/blob/v2.3.1/setup.py#L341
 export PACKAGE_TYPE=conda
 
+export CMAKE_FIND_DEBUG_MODE=1
+export USE_NUMPY=1
 
 # hmaarrfk - Designed for Pytorch 2.4.0
 # Help detect failures in Numpy Early
