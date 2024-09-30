d3856e3ae47d17532e1d47ae6805e0add904f98a
Mark Harfouche <mark.harfouche@gmail.com>
2024-09-30 16:34:35 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,11 @@ set -ex
 
 mkdir build
 cd build
-# Python3_NumPy_INCLUDE_DIR="$(python -c 'import numpy;print(numpy.get_include())')"
-export Python3_NumPy_INCLUDE_DIRS="$(python -c 'import numpy;print(numpy.get_include())')"
+Python3_NumPy_INCLUDE_DIR="$(python -c 'import numpy;print(numpy.get_include())')"
+# export Python3_NumPy_INCLUDE_DIRS="$(python -c 'import numpy;print(numpy.get_include())')"
 
-# Python_NumPy_INCLUDE_DIR="$(python -c 'import numpy;print(numpy.get_include())')"
-export Python_NumPy_INCLUDE_DIRS="$(python -c 'import numpy;print(numpy.get_include())')"
+Python_NumPy_INCLUDE_DIR="$(python -c 'import numpy;print(numpy.get_include())')"
+# export Python_NumPy_INCLUDE_DIRS="$(python -c 'import numpy;print(numpy.get_include())')"
 cmake ${CMAKE_ARGS} ..
 
 echo it worked
