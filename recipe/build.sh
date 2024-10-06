b6e2d2e3b9b0f2bc79d2ae6d85df1fbe2ebca5b3
Mark Harfouche <mark.harfouche@gmail.com>
2024-10-05 20:39:02 -0400

    do one more replacement

--- recipe/build.sh
+++ recipe/build.sh
@@ -107,7 +107,9 @@ fi
 
 if [[ "$PKG_NAME" == "pytorch" ]]; then
   PIP_ACTION=install
-  sed "s/3.12/$PY_VER/g" build/CMakeCache.txt.orig > build/CMakeCache.txt
+  # Trick Cmake into thinking python hasn't changed
+  sed "s/3\.12/$PY_VER/g" build/CMakeCache.txt.orig > build/CMakeCache.txt
+  sed -i.bak "s/3;12/${PY_VER%.*};${PY_VER#*.}/g" build/CMakeCache.txt
 else
   # For the main script we just build a wheel for so that the C++/CUDA
   # parts are built. Then they are reused in each python version.
