bd450c91075faceaf5ce7b13dc5a48826ac9d5a4
Daniel Petry <dpetry@anaconda.com>
2025-01-24 14:53:42 -0600

    Revert "Hand contents of CMAKE_ARGS directly to CMake, to remove TORCH_CUDA_ARCH_LIST warning and need to export them all individually"
    This reverts commit f3bfe5f6aa923e7b0ff3ae01b5528d1f11532297.

--- recipe/build.sh
+++ recipe/build.sh
@@ -233,7 +233,7 @@ case ${PKG_NAME} in
     cp build/CMakeCache.txt build/CMakeCache.txt.orig
     ;;
   pytorch)
-    $PREFIX/bin/python -m pip install . --no-deps --no-build-isolation --config-settings="cmake.args=${CMAKE_ARGS}" -vvv --no-clean \
+    $PREFIX/bin/python -m pip install . --no-deps --no-build-isolation -vvv --no-clean \
         | sed "s,${CXX},\$\{CXX\},g" \
         | sed "s,${PREFIX},\$\{PREFIX\},g"
     # Keep this in ${PREFIX}/lib so that the library can be found by
