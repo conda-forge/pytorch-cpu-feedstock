44782b3f4b6745ccca97b04b8e2a636edc567379
H. Vetinari <h.vetinari@gmx.com>
2025-02-02 18:25:48 +1100

    also switch off very noisy ptxas warnings
    we get 10'000s of lines of
    ```
    ptxas /tmp/tmpxft_00006937_00000000-8_SparseSemiStructuredOps.compute_86.ptx, line 8606; info    : Advisory: Modifier '.sp::ordered_metadata' should be used on instruction 'mma' instead of modifier '.sp' as it is expected to have substantially reduced performance on some future architectures
    ```
    We cannot directly talk to ptxas, we need to pass this to nvcc, see
    https://docs.nvidia.com/cuda/cuda-compiler-driver-nvcc/index.html#ptxas-options-options-xptxas

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -94,7 +94,7 @@ if not "%cuda_compiler_version%" == "None" (
     set "PATH=%CUDA_BIN_PATH%;%PATH%"
     set CUDNN_INCLUDE_DIR=%LIBRARY_PREFIX%\include
     @REM turn off very noisy nvcc warnings
-    set "CUDAFLAGS=-w"
+    set "CUDAFLAGS=-w --ptxas-options=-w"
 ) else (
     set USE_CUDA=0
     @REM MKLDNN is an Apache-2.0 licensed library for DNNs and is used
