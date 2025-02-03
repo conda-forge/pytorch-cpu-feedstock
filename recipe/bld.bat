0fbd21f57925fdb25bc347f5522106e5f422f8e1
H. Vetinari <h.vetinari@gmx.com>
2025-02-04 07:13:36 +1100

    drop unnecessary CUDAFLAGS in bld.bat
    this was done for consistency with `build.sh`, but appears unnecessary on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -94,8 +94,6 @@ if not "%cuda_compiler_version%" == "None" (
     set "PATH=%CUDA_BIN_PATH%;%PATH%"
     set CUDNN_INCLUDE_DIR=%LIBRARY_PREFIX%\include
     set "CUDA_VERSION=%cuda_compiler_version%"
-    @REM turn off very noisy nvcc warnings
-    set "CUDAFLAGS=-w --ptxas-options=-w"
 ) else (
     set USE_CUDA=0
     @REM MKLDNN is an Apache-2.0 licensed library for DNNs and is used
