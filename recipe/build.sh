d2dbaa308c9b843c7feb93a80709541b4fe67f04
H. Vetinari <h.vetinari@gmx.com>
2025-02-04 06:16:23 +1100

    switch to CMAKE_CUDA_FLAGS to avoid spurious rebuilds due to cache issues
    Co-Authored-By: Michał Górny <mgorny@gentoo.org>
    Debugged-By: Michał Górny <mgorny@gentoo.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -220,7 +220,7 @@ elif [[ ${cuda_compiler_version} != "None" ]]; then
     export MAGMA_HOME="${PREFIX}"
     export USE_MAGMA=1
     # turn off noisy nvcc warnings
-    export CUDAFLAGS="-w --ptxas-options=-w"
+    export CMAKE_CUDA_FLAGS="-w --ptxas-options=-w"
 else
     if [[ "$target_platform" != *-64 ]]; then
       # Breakpad seems to not work on aarch64 or ppc64le
