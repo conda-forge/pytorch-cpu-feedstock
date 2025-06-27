3ef33336d112828b9eed1078b2e5689590fbc3f5
H. Vetinari <h.vetinari@gmx.com>
2025-06-27 21:43:58 +1100

    make use of SET_PAGEFILE in ci-setup on windows

--- .scripts/run_win_build.bat
+++ .scripts/run_win_build.bat
@@ -53,6 +53,9 @@ echo Setting up configuration
 setup_conda_rc .\ ".\recipe" .\.ci_support\%CONFIG%.yaml
 if !errorlevel! neq 0 exit /b !errorlevel!
 echo Running build setup
+:: manual work-around because we can't pass SET_PAGEFILE for non-azure CI
+:: through configuration/generation in conda-smithy yet
+set "SET_PAGEFILE=True"
 CALL run_conda_forge_build_setup
 
 
