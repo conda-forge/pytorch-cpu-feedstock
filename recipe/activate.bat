e4b5b386429017b629706bfb3d6e6e683918748c
Daniel Ching <9604511+carterbox@users.noreply.github.com>
2025-12-16 11:36:55 -0600

    DOC: Add explanatory comment to activation script

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -2,6 +2,8 @@
 
 if not defined CF_TORCH_CUDA_ARCH_LIST (
     set "CF_TORCH_CUDA_ARCH_LIST=@cf_torch_cuda_arch_list@"
+    :: "NOT_SET" is used as the value because it is clearer (explicit) that the activation 
+    :: script was run and found no previous value for "CF_TORCH_CUDA_ARCH_LIST".
     set "CF_TORCH_CUDA_ARCH_LIST_BACKUP=NOT_SET"
 )
 
